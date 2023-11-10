7e74f46cc00727829b40909617eea9e0fc2dc1f9
Dmitry Dorofeev <DmitryDorofeev@users.noreply.github.com>
2023-11-10 14:54:58 -0500

    fix: opening iframe (#3)

--- Sources/TruvSDK/Classes/Models/TruvEventPayload.swift
+++ Sources/TruvSDK/Classes/Models/TruvEventPayload.swift
@@ -44,10 +44,5 @@ public struct TruvEventPayload: Codable {
         case unsupportedBrowser = "UNSUPPORTED_BROWSER"
         case close = "CLOSE"
     }
-    
-    var dictionary: [String: Any]? {
-    guard let data = try? JSONEncoder().encode(self) else { return nil }
-    return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
-    }
 
 }
--- Sources/TruvSDK/Classes/Models/TruvSuccessPayload.swift
+++ Sources/TruvSDK/Classes/Models/TruvSuccessPayload.swift
@@ -20,11 +20,4 @@ public struct TruvSuccessPayload: Codable {
         case metadata
     }
     
-    
-    var dictionary: [String: Any]? {
-    guard let data = try? JSONEncoder().encode(self) else { return nil }
-    return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
-    }
-    
-    
 }
